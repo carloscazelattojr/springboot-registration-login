53cc4c280c1d26fc540a290afc0d69493e3c7704
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-06-17 10:33:26 -0300

    Created JPA Entities: User and Role (and yours relations) and bug fix in configs

---
+++ src/main/java/br/com/carlosjunior/registrationlogin/entities/User.java
@@ -0,0 +1,102 @@
+package br.com.carlosjunior.registrationlogin.entities;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "tb_user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firsName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String email;
+
+	private String password;
+
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(	name = "tb_users_roles",
+				joinColumns = @JoinColumn(
+						name = "user_id", referencedColumnName = "id"),
+				inverseJoinColumns = @JoinColumn(
+						name = "roler_id", referencedColumnName = "id")
+				)
+	private Collection<Role> roles;
+
+	public User(String firsName, String lastName, String email, String password, Collection<Role> roles) {
+		super();
+		this.firsName = firsName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirsName() {
+		return firsName;
+	}
+
+	public void setFirsName(String firsName) {
+		this.firsName = firsName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Role> roles) {
+		this.roles = roles;
+	}
+
+}
